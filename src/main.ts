a3128528a20d1408ac70277ae8829acbfddc6e4e
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-12-11 16:25:54 -0300

    Banco

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,55 @@
 import express, { Router } from "express";
 import cors from "cors";
+import mysql from "mysql2";
+// get the client
+
+// create the connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "fundacao",
+});
+
+// // simple query
+// connection.query(
+//   'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//   function(err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );
+
+// // with placeholder
+// connection.query(
+//   'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//   ['Page', 45],
+//   function(err, results) {
+//     console.log(results);
+//   }
+// );
 
 const app = express();
 const port = 3090;
 
 app.use(cors());
 
+app.use(express.json());
+
 const router: Router = Router();
 
-router.get("/", (req, res) => {
-  res.send({
-    texto: ["Bolsonaro", "sla", "opa"],
-  });
+router.get("/", async (req, res) => {
+  const result = await connection.promise().query("SELECT * FROM Pessoa");
+  res.send(result[0]);
 });
 
-router.post("/teste",async (req, res) => {
-  const count = req.query;
-  console.log(count);
+router.post("/", async (req, res) => {
+  const result = await connection
+    .promise()
+    .query(
+      `INSERT INTO fundacao.pessoa (nome, idade) VALUES ('POST', ${req.body.numero})`
+    );
+  console.log(req.body.numero);
   res.send("ok");
 });
 
